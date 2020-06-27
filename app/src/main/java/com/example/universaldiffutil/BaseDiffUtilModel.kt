c9cd06b5bdd5d4e43a8a3b27faf664b832ac5b79
yogesh_gtsinfosoft <yogesh@gtsinfosoft.com>
2020-06-27 18:22:11 +0530

    initial commit

--- app/src/main/java/com/example/universaldiffutil/BaseDiffUtilModel.kt
+++ app/src/main/java/com/example/universaldiffutil/BaseDiffUtilModel.kt
@@ -1,5 +1,9 @@
 package com.example.universaldiffutil
 
+/**
+ * @author Yogesh Paliyal
+ * @since 27 June 2020
+ */
 abstract class BaseDiffUtilModel {
     public abstract fun getId() :String
 }
--- app/src/main/java/com/example/universaldiffutil/UniversalDiffUtil.kt
+++ app/src/main/java/com/example/universaldiffutil/UniversalDiffUtil.kt
@@ -2,6 +2,10 @@ package com.example.universaldiffutil
 
 import androidx.recyclerview.widget.DiffUtil
 
+/**
+ * @author Yogesh Paliyal
+ * @since 27 June 2020
+ */
 class UniversalDiffUtil<T : BaseDiffUtilModel>(val oldList : List<T>, val newList : List<T>) : DiffUtil.Callback() {
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
