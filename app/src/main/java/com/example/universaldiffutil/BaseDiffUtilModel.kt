d475c3932172c2d4c3859d7923d959081b1c3829
yogesh_gtsinfosoft <yogesh@gtsinfosoft.com>
2020-06-27 18:18:12 +0530

    initial commit

---
+++ app/src/main/java/com/example/universaldiffutil/BaseDiffUtilModel.kt
@@ -0,0 +1,5 @@
+package com.example.universaldiffutil
+
+abstract class BaseDiffUtilModel {
+    public abstract fun getId() :String
+}
---
+++ app/src/main/java/com/example/universaldiffutil/UniversalDiffUtil.kt
@@ -0,0 +1,23 @@
+package com.example.universaldiffutil
+
+import androidx.recyclerview.widget.DiffUtil
+
+class UniversalDiffUtil<T : BaseDiffUtilModel>(val oldList : List<T>, val newList : List<T>) : DiffUtil.Callback() {
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].getId() == newList[newItemPosition].getId()
+    }
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].toString() == newList[newItemPosition].toString()
+    }
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        return super.getChangePayload(oldItemPosition, newItemPosition)
+    }
+
+}
